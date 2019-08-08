6d20ee57798d0715ef8affec3fede0fd7eee3c96
Mitchell <mitchell.rathgeb@gmail.com>
2019-08-08 14:04:26 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+
+  // arrow function binds the method to the lexical scope in which it is defined, in this case, the App class (meaning no need for this.bind in the constuctor)
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
+render() {
+
+  const { monsters, searchField } = this.state;
+  // same as doing: 
+  // const monsters = this.state.monsters;
+  // const searchField = this.state.searchField;
+  //(destructuring)
+  const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox 
+        placeholder={'search monsters'}
+        handleChange={this.handleChange}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
+ }
 }
 
 export default App;
