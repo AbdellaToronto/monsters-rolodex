ed2ee5ca2c8a0f03495a4e215342671af9cd0455
Mitchell <mitchell.rathgeb@gmail.com>
2019-08-08 14:03:18 -0400

    Updates

---
+++ precache-manifest.2e080bf01c1ab18bceafe59ea3403117.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023378ef5b44a6a2ce6a0957a82c4030",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "39eb022bba2cee5d89a5",
+    "url": "/monster-rolodex/static/css/main.c75acadd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "39eb022bba2cee5d89a5",
+    "url": "/monster-rolodex/static/js/main.e9687367.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
